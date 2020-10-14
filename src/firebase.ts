bf1a41d32526a59e6dd3ac4b20246eed5b8f8616
akeamwoods <a19941@hotmail.co.uk>
2020-10-14 20:58:02 +0100

    added environment env

---
+++ src/firebase.ts
@@ -0,0 +1,36 @@
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+import { eventChannel } from "redux-saga";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
+
+const auth = firebase.auth();
+
+export const login = (email: string, password: string) =>
+  void auth
+    .signInWithEmailAndPassword(email, password)
+    .catch(function (error: any) {
+      console.log(error);
+    });
+
+export const logout = () => void auth.signOut();
+
+export const subscribeToAuth = () =>
+  eventChannel((emit) => {
+    auth.onAuthStateChanged((user) => {
+      emit(user ? { email: user.email, id: user.uid } : false);
+    });
+    return () => {
+      console.log("destroy chan auth");
+    };
+  });
