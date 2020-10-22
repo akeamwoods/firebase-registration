a18d6069e4d0c18e0655c0f4089fdda7cdef0459
akeamwoods <a19941@hotmail.co.uk>
2020-10-22 20:36:09 +0100

    improved styled component syntax

--- src/components/Avatar/style.ts
+++ src/components/Avatar/style.ts
@@ -1,17 +1,13 @@
 import styled from "styled-components";
 
-type AvatarProps = {circle:boolean};
-
-export const Image = styled.img.attrs((props: AvatarProps) => ({}))<
-AvatarProps
->`
+export const Image = styled.img<{circle: boolean}>`
+${props => props.circle && `
+    border-radius: 50%;
+  `}
   padding: 0;
   margin: 0;
   vertical-align: middle;
   object-fit: cover;
   object-position: 50% 50%;
   align-self:center;
-  border-radius: ${(props) => (props.circle ? "50%" : "none")};
 `;
-
-
